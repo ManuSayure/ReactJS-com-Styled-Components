9e99b18f6ae022acf54b938dcd9b6f863af39da6
Manoela Cassia <manoelacs@ic.ufal.br>
2021-01-13 10:55:53 -0300

    Updates

---
+++ precache-manifest.9135b68dc7aa2696249c9400de1cab7c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d02e4cb9bcbccf558427a10c67602b0f",
+    "url": "/ReactJS-com-Styled-Components/index.html"
+  },
+  {
+    "revision": "9b4a6165cc733c4dbe94",
+    "url": "/ReactJS-com-Styled-Components/static/js/2.2ae298c6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ReactJS-com-Styled-Components/static/js/2.2ae298c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15d3d579917520350736",
+    "url": "/ReactJS-com-Styled-Components/static/js/main.105da331.chunk.js"
+  },
+  {
+    "revision": "c4182f92904b518dbdd3",
+    "url": "/ReactJS-com-Styled-Components/static/js/runtime-main.77b01142.js"
+  },
+  {
+    "revision": "44dbaa78fb2ed240792c9e7a424b0ea0",
+    "url": "/ReactJS-com-Styled-Components/static/media/alimentacao.44dbaa78.svg"
+  },
+  {
+    "revision": "f3a2bff49642ca68b3841fde422295cb",
+    "url": "/ReactJS-com-Styled-Components/static/media/bank_logo.f3a2bff4.svg"
+  },
+  {
+    "revision": "b97c300bed11302a8687c16e07aa89e5",
+    "url": "/ReactJS-com-Styled-Components/static/media/dinheiro.b97c300b.svg"
+  },
+  {
+    "revision": "acd41ceb3366a71c61b7ed3d5df83682",
+    "url": "/ReactJS-com-Styled-Components/static/media/olho.acd41ceb.svg"
+  },
+  {
+    "revision": "17e95f3cc45dc23ffda7f4d29dc137df",
+    "url": "/ReactJS-com-Styled-Components/static/media/outros.17e95f3c.svg"
+  },
+  {
+    "revision": "134d853a95b2b30676da2c63cda2eeaf",
+    "url": "/ReactJS-com-Styled-Components/static/media/privado.134d853a.svg"
+  },
+  {
+    "revision": "605ef9ca7e70a1bb568ffcec443d05f6",
+    "url": "/ReactJS-com-Styled-Components/static/media/saude.605ef9ca.svg"
+  },
+  {
+    "revision": "0981f5bfd5f197c52e678c3c0616cd81",
+    "url": "/ReactJS-com-Styled-Components/static/media/themeOff.0981f5bf.svg"
+  },
+  {
+    "revision": "75ea28146734a5ea6593dab45d272a90",
+    "url": "/ReactJS-com-Styled-Components/static/media/themeOn.75ea2814.svg"
+  },
+  {
+    "revision": "15a4ed7310630a72e21a8054897c3a51",
+    "url": "/ReactJS-com-Styled-Components/static/media/transporte.15a4ed73.svg"
+  },
+  {
+    "revision": "59035811601fe0a607827eb6408e14ef",
+    "url": "/ReactJS-com-Styled-Components/static/media/utilidades.59035811.svg"
+  }
+]);
